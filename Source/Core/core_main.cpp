efb77fd907e8983739619de111122bfaebd7b4f0
megamilkx <gyrocopter3k@gmail.com>
2018-08-12 20:16:02 +0300

    Remove unused macro

--- Source/Core/core_main.cpp
+++ Source/Core/core_main.cpp
@@ -14,8 +14,6 @@
 #include <text_mesh.h>
 #include <asset.h>
 
-#define REG_COMP(NAME) SceneObject::RegisterComponent<NAME>(#NAME)
-
 void Aurora2Init();
 
 int main()
@@ -23,16 +21,6 @@ int main()
     init_resources();
 
     Meta::Print();
-
-    REG_COMP(Transform);
-    REG_COMP(Model);
-    REG_COMP(Skeleton);
-    REG_COMP(Renderer);
-    REG_COMP(LuaScript);
-    REG_COMP(LightOmni);
-    REG_COMP(Camera);
-    REG_COMP(Animator);
-    REG_COMP(SoundEmitter);
 
     GameState::Init();
     Aurora2Init();
