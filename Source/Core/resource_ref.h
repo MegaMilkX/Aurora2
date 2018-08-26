4c2989c62ae3a9b10a06bd24cc8758267fc99637
megamilkx <gyrocopter3k@gmail.com>
2018-08-26 23:44:59 +0300

    Add resource ref callbacks

--- Source/Core/resource_ref.h
+++ Source/Core/resource_ref.h
@@ -6,21 +6,43 @@
 class ResourceRef
 {
 public:
+    typedef std::function<void(ResourceRef* ref)> resource_ref_changed_cb_t;
+
     ResourceRef() {}
     ResourceRef(const std::string& name) 
     : name(name) {}
     operator bool() const {
         return g_resourceRegistry.Exists(name);
     }
-    void Set(const std::string& name) { this->name = name; }
+    void operator=(const ResourceRef& other) {
+        std::cout << "operator= " << other.name << std::endl;
+        Set(other.name);
+    }
+    void Set(const std::string& name) 
+    { 
+        std::cout << "Set " << name << std::endl;
+        this->name = name;
+        Changed(); 
+    }
     const std::string& GetTargetName() const { return name; }
     template<typename T>
     T* Get() {
         return g_resourceObjectRegistry.Get<T>(name);
     }
     ResourceRaw* GetRaw() { return g_resourceRegistry.Get(name); }
+
+    void AddChangeCallback(resource_ref_changed_cb_t cb) {
+        callbacks.emplace_back(cb);
+    }
 private:
+    void Changed() {
+        for(auto& cb : callbacks) {
+            std::cout << "Calling a callback" << std::endl;
+            cb(this);
+        }
+    }
     std::string name;
+    std::vector<resource_ref_changed_cb_t> callbacks;
 };
 
 #endif
